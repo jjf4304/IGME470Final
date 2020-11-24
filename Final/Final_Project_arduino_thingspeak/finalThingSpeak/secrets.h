67120e47d6edcf524221404900028f1f15500610
jjf4304 <54686120+jjf4304@users.noreply.github.com>
2020-11-24 07:59:35 -0500

    Update secrets.h

--- Final/Final_Project_arduino_thingspeak/finalThingSpeak/secrets.h
+++ Final/Final_Project_arduino_thingspeak/finalThingSpeak/secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID "Verizon-SM-G973U-254d"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "arduinoplswork"  // replace MyPassword with your WiFi password
+#define SECRET_SSID ""    // replace MySSID with your WiFi network name
+#define SECRET_PASS ""  // replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 1238932      // replace 0000000 with your channel number
 #define SECRET_WRITE_APIKEY "1GZ7WW9ZB6PU3KY1"   // replace XYZ with your channel write API Key
