774a8ab4c0bad1b733857465007d003ce1403888
jjf4304 <54686120+jjf4304@users.noreply.github.com>
2020-11-24 08:00:03 -0500

    Update secrets.h

--- Final/Final_Project_arduino/secrets.h
+++ Final/Final_Project_arduino/secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID "fredrickson"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "1234567890"  // replace MyPassword with your WiFi password
+#define SECRET_SSID ""    // replace MySSID with your WiFi network name
+#define SECRET_PASS ""  // replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 1238932      // replace 0000000 with your channel number
 #define SECRET_WRITE_APIKEY "1GZ7WW9ZB6PU3KY1"   // replace XYZ with your channel write API Key
